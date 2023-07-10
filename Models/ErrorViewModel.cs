ec916696fcfdf515bab84bcb22916832b9247057
Tuna Shawn <tunashawnv@gmail.com>
2023-07-10 15:21:09 +0700

    added login

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace employee_manager_aspnetcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
